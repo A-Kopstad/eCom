d019f192a11f4312112c40d195f64826386dbc82
A-Kopstad <126067888+A-Kopstad@users.noreply.github.com>
2024-10-09 10:13:57 +0200

    deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-  },
-  base: '/', // Change this if your app is under a specific path
-});
+    outDir: 'build'  // Change output directory from 'dist' to 'build'
+  }
+})
